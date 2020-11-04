f89a81ac463789529c350d0560bf7b19cbfc5ac2
Emily Gann <aspentree79@gmail.com>
2020-11-04 16:39:27 -0500

    created js and html files, added DOM event listener and getEvents function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Howdy, index.js");
+const endPoint = "http://localhost:3000/api/v1/events";
+
+document.addEventListener('DOMContentLoaded', () => {
+    getEvents()
+});
+
+function getEvents() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(events => {
+        console.log(events);
+    })
+}
